4d569e19ecf302a6fb0826e0e9a9b14c7c8b2913
Sebastian <sebastiansierra1311@gmail.com>
2024-07-28 23:07:00 -0500

    creacion de carpeta docs

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,3 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-ZU2NHLOL.mjs";import"./chunk-KYUKFCN3.mjs";import"./chunk-WP7XHKMF.mjs";export{a as default};
+//# sourceMappingURL=main.server.mjs.map
